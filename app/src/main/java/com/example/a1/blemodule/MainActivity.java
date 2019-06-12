1eb08e5b5cd4f92583116e994aaf89877ce9f3cb
noobiny <dkgbdlfjs@gmail.com>
2019-06-12 18:15:19 +0900

    스레드 테스트

--- app/src/main/java/com/example/a1/blemodule/MainActivity.java
+++ app/src/main/java/com/example/a1/blemodule/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a1.blemodule;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -21,6 +22,10 @@ public class MainActivity extends AppCompatActivity implements ItemFragment.OnLi
     EditText edit;
     Button btn_scan, btn_write, btn_start, btn_stop;
     BluetoothModule bluetoothModule = BluetoothModule.getInstance();
+    Thread thread;
+    Runnable runnable;
+    Handler handler = new Handler();
+    boolean started = true;
     private int DEVCIE_LIST_BT_ACTIVITY = 99;
 
     @Override
@@ -36,18 +41,82 @@ public class MainActivity extends AppCompatActivity implements ItemFragment.OnLi
         btn_start = findViewById(R.id.btn_start);
         btn_stop = findViewById(R.id.btn_stop);
 
-
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Thread th = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int Num = 1;
+                        while (started) {
+
+                            bluetoothModule.sendProtocol("rxlg0", new BluetoothModule.BluetoothWriteImpl() {
+                                @Override
+                                public void onSuccessWrite(int status, String data) {
+                                    tv2.setText(tv2.getText() + "\n" + data);
+                                }
+
+                                @Override
+                                public void onFailed(Exception e) {
+
+                                }
+                            });
+
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                started=true;
+                th.start();
+
+//                runnable = new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        while (started) {
+//                            handler.post(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    bluetoothModule.sendProtocol("rxlg0", new BluetoothModule.BluetoothWriteImpl() {
+//                                        @Override
+//                                        public void onSuccessWrite(int status, String data) throws IOException {
+//                                            tv2.setText(tv2.getText() + "\n" + data);
+//                                        }
+//
+//                                        @Override
+//                                        public void onFailed(Exception e) {
+//
+//                                        }
+//                                    });
+//                                }
+//                            });
+//
+//                            try {
+//                                Thread.sleep(3000); //10초
+//                            } catch (InterruptedException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }
+//                };
+
+
+                thread = new Thread(runnable);
+                thread.start();
             }
         });
 
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                started = false;
+//                thread.interrupt();
+                thread.interrupt();
             }
         });
 
